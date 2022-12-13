06e61cf3305384a5876e394194cd520a3d719282
JoeSpencer1 <108445563+JoeSpencer1@users.noreply.github.com>
2022-12-12 21:27:38 -0700

    Added Factorial.

---
+++ Factorial/factorial.h
@@ -0,0 +1,18 @@
+#ifndef FACTORIAL
+#define FACTORIAL
+
+#include <vector>
+
+class factorial
+{
+public:
+    factorial();
+    int fact();
+    int pow();
+    void graph(int last);
+private:
+    int current;
+    int factor;
+};
+
+#endif
