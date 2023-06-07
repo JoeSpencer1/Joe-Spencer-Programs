f55f5f6a8a6da3ef36b354608bd53676991045ef
JoeSpencer1 <108445563+JoeSpencer1@users.noreply.github.com>
2023-06-07 15:28:34 -0600

    It works pretty well! The only thing that might need cleaned up is some capitalized words in the word list like RADAR. I think this is not a huge problem, though.

--- Wordle/Wordle.py
+++ Wordle/Wordle.py
@@ -1,9 +1,10 @@
 import numpy as np
 import string
 
-# If n is increased or decreased, the word clas will need to be changed too.
-## Working on fixing that.
+# If n is increased or decreased, there may be more than 3 repeated letters in words.
+# This would have to be fixed manually.
 n = 5
+tries = 6
 
 # Create word class for a linked list of all the words.
 class Word:
@@ -18,7 +19,6 @@ class Word:
         for i in range(n):
             ret = ret + self.letters[i]
         return ret
-
 
 class WordsList:
     def __init__(self):
@@ -52,27 +52,34 @@ class WordsList:
             current = current.next
         return current
     
-    def highScore(self, fre, typ):
-        if typ == 1:
-            high = 0
-            loc = 0
-            current = self.head
-            while current:
-                score = 0
-                list = []
-                for i in current.letters:
-                    rep = 0
-                    for let in list:
-                        if let == i:
-                            rep += 1
-                    list.append(i)
-                    score += fre[i][3 * n + rep]
-                if score > high:
-                    high = score
-                    loc = current.position
-                current = current.next
+    def highScore(self, fre):
+        high = 0
+        loc = 0
+        current = self.head
+        while current:
+            score = 0
+            list = []
+            for i in current.letters:
+                rep = 0
+                for let in list:
+                    if let == i:
+                        rep += 1
+                list.append(i)
+                score += fre[i][3 * n + rep]
+            if score > high:
+                high = score
+                loc = current.position
+            current = current.next
         return loc
     
+    def length(self):
+        current = self.head
+        l = 0
+        while current:
+            l += 1
+            current = current.next
+        return l
+
     def nWords(self, current, num):
         current = self.head
         while current:
@@ -83,7 +90,6 @@ class WordsList:
     def returnN(self, loc):
         current = self.head
         while current.position < loc:
-            #print(current.letters)
             current = current.next
         retstr = ''
         for a in current.letters:
@@ -92,7 +98,6 @@ class WordsList:
 
     def remove(self, current, rem):
         if rem == False:
-            #print("false")
             return current.next
         else:
             if current.prev == None:
@@ -105,21 +110,21 @@ class WordsList:
                     current.next.prev = current.prev
         return current.next
 
-    def removeL(self, pos, ent, res, freq):
+    def removeL(self, pos, ent, resa, freq):
         rem = False
+        res = resa[pos * 2]
         current = self.head
         while current:
             rem = False
             if res == '-':
                 for i in range(len(current.letters)):
-                    #print('-', current.letters[i])
                     if current.letters[i] == ent:
-                        rem = True
+                        if resa[i * 2] != ent:
+                            rem = True
             if res == '*':
                 rem = True
                 rem2 = False
                 for i in range(n):
-                    #print('*', current.letters[i])
                     if current.letters[i] == ent:
                         rem = False
                         if i == pos:
@@ -128,9 +133,6 @@ class WordsList:
                     rem = True
             if res.islower() and current.letters[pos] != ent:
                 rem = True
-                #print('islower', ent, current.letters[pos])
-            #if current.letters.all == 'alter':
-                #print('Not deleted')
             if rem == True:
                 loc = 0
                 for letter in current.letters:
@@ -146,7 +148,6 @@ class WordsList:
                     if rep > 1:
                         freq[current.letters[loc]][loc + 2 * n] -= 1
                         freq[current.letters[loc]][3 * n + 2] -= 1
-                    print(rep)
                     loc += 1
             current = self.remove(current, rem)
         return freq
@@ -154,9 +155,6 @@ class WordsList:
 # Creat an array of all the words.
 file = open("Words.txt")
 rfile = file.read()
-#print(rfile[0:n])
-#print(rfile[6:10])
-#print(rfile[len(rfile) - n:len(rfile) - 1])
 
 # Create a hash table of "scores" for each letter at each spot for each letter.
 words = WordsList()
@@ -170,9 +168,12 @@ print("This code will estimate the best word to enter.")
 print("An \"-\" indicates a letter not in the word, a \"*\" is in the wrong place,")
 print("and outputting the letter itself that it is correct.")
 
+resp = input("Would you like to display all possible words? (y/n)\n")
+if resp != 'y' and resp != 'n':
+    resp = input("Please enter a valid lower case y/n response:\n")
+
 # Add all letters to the hash table
 for i in range(len(rfile)):
-    #print(i, rfile[i])
     if i % (n + 1) == 0:
         read = np.empty(shape = 5, dtype = str)
         for j in range(n):
@@ -181,10 +182,8 @@ for i in range(len(rfile)):
     if (i + 1) % (n + 1) != 0:
         rep = 0
         for j in range(i - (i % (n + 1)), i):
-            #print(j, ' ', rfile[j])
             if rfile[j] == rfile[i]:
                 rep += 1
-                #print("REP", i, ' ', j, rep)
         freq[rfile[i]][i % n] += 1
         freq[rfile[i]][3 * n] += 1
         if rep > 0:
@@ -196,38 +195,36 @@ for i in range(len(rfile)):
         # This code assumes letters are not repeated more than 3 times.
         # It could have to be adjusted for other versions of Wordle.
 
+for guess in range(tries):
+    # Find out which word has the highest score
+    loc = words.highScore(freq)
+    test = words.findPosition(loc).returnL()
+    print('\n', test, '\n')
+    valid = False
 
-#print('f', freq['f'][:])
-#words.displayAll()
-#print(words.nWords(words.head, 0))
-#print(words.returnL())
-#print(words.findPosition(100).returnL())
-
-# Find out which word has the highest score
-loc = words.highScore(freq, 1)
-#print(loc)
-test = words.findPosition(loc).returnL()
-print(test)
-valid = False
-
-# Obtain results and determine which letters to remove.
-while valid == False:
-    valid = True
-    result = input("Please enter the results of this wordle guess:\n")
-    for i in range(n * 2 - 1):
-        if ((i % 2) == 0) & (valid == True) & (result[i] != test[int(i / 2)]) & (result[i] != '*') & (result[i] != '-'):
+    # Obtain results and determine which letters to remove.
+    while valid == False:
+        valid = True
+        result = input("Please enter the results of this wordle guess:\n")
+        if len(result) != n * 2 - 1:
             valid = False
-            print("n")
-        if ((i & 2) == 1) & (valid == True) & (result[i] != ' '):
-            valid = False
-            print("space ", i)
-    if valid == False:
-        print("Please provide a correct input of letters")
+        else:
+            for i in range(n * 2 - 1):
+                if ((i % 2) == 0) & (valid == True) & (result[i] != test[int(i / 2)]) & (result[i] != '*') & (result[i] != '-'):
+                    valid = False
+                if ((i % 2) == 1) & (valid == True) & (result[i] != ' '):
+                    valid = False
+        if valid == False:
+            print("Please provide a correct input of letters")
 
-# Return results to program
-for i in range(n):
-    freq = words.removeL(i, test[i], result[i * 2], freq)
-#words.displayAll()
-#print(words.nWords(words.head, 0))
-#for a in abc:
-    #print(freq[a][15:18])
+    # Return results to program
+    for i in range(n):
+        freq = words.removeL(i, test[i], result, freq)
+    if resp == 'y':
+        words.displayAll()
+    if words.length() < 2:
+        print('We found it!')
+        words.displayAll()
+        break
+    if guess == tries:
+        print('Sorry, we did not find it in time.')
